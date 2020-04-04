57b3d729c9f0a8662755453e2e093066cf12c871
Lucianovici <lucian.corduneanu@sensidev.com>
2020-04-05 02:18:03 +0300

    Clear sandbox

--- apps/sandbox/main.cpp
+++ apps/sandbox/main.cpp
@@ -3,31 +3,5 @@
 using namespace std;
 
 int main() {
-    int matrix[10][10] = {};
-
-    for (int lineIndex = 0; lineIndex < 10; lineIndex++) {
-        int *linePointer = matrix[lineIndex];
-
-        //continutul pointerului va fii setat cu lineIndex + 1
-        *linePointer = lineIndex + 1;
-
-        for (int colIndex = 0; colIndex < 9; colIndex++) {
-            int *lineCurrItemPointer = linePointer + colIndex;
-            int *lineNextItemPointer = linePointer + colIndex + 1;
-            *lineNextItemPointer = *lineCurrItemPointer + *linePointer;
-
-            if(linePointer == lineCurrItemPointer) {
-                cout << "Match line with current item on line "<< lineIndex << " and col " <<colIndex << endl;
-            }
-        }
-    }
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            cout.width(4);
-            cout << matrix[i][j];
-        }
-        cout << endl;
-    }
     return 0;
 }
